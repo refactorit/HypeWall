2822e7b6fcd809c958c5c802812e6b36ae175648
Filip Defar <filip@codemonster.io>
2014-09-22 20:37:49 +0200

    Code cleanup and refactoring

--- server.js
+++ server.js
@@ -1,36 +1,32 @@
 var express = require('express');
 var app = express();
 
-app.get('/hello', function(req, res){
-  res.sendfile(__dirname + '/hello.html');
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/template.html');
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(9090, function() {
   console.log('Listening on port %d', server.address().port);
 });
 
-var Twitter = require('node-tweet-stream')
-  , t = new Twitter({
-    consumer_key: 'JSwd0sFkVGunfvAwrvSlECZEB',
-    consumer_secret: 'Ezz0OHpFylLSEYrJOmtk7txRyxZMjIoguCUFNEer3Lh6CeNRmu',
-    token: '117047483-ejjH2c3lR9gKHy0Z4uiBsQppJIDS6z8U7MHkNjlA',
-    token_secret: 'PmZofv32MoNs1MUZcvlSwzOFG4O6avsQqaAbr8eAGn3Jr'
-  })
+var Twitter = require('node-tweet-stream');
+var t = new Twitter({
+  consumer_key: 'JSwd0sFkVGunfvAwrvSlECZEB',
+  consumer_secret: 'Ezz0OHpFylLSEYrJOmtk7txRyxZMjIoguCUFNEer3Lh6CeNRmu',
+  token: '117047483-ejjH2c3lR9gKHy0Z4uiBsQppJIDS6z8U7MHkNjlA',
+  token_secret: 'PmZofv32MoNs1MUZcvlSwzOFG4O6avsQqaAbr8eAGn3Jr'
+});
 
 t.on('error', function (err) {
-  console.log('Oh no')
-})
+  console.log('Error!')
+});
 
-t.track('nodejs')
-t.track('3YearsOfUnbroken')
+t.track('london')
 
 var io = require('socket.io')(server);
 io.on('connection', function (socket) {
   t.on('tweet', function (tweet) {
-    console.log('tweet received', tweet)
-    socket.emit('news', tweet.text);
+    console.log('New tweet: ', tweet.text)
+    socket.emit('news', tweet);
   })
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
 });
