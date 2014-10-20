2ad1d58767a4a6d7a13f18e97bcdf86b13e07a20
Filip Defar <filip@codemonster.io>
2014-10-20 13:41:11 +0200

    Moved config to own file

---
+++ config.js
@@ -0,0 +1,10 @@
+exports.twitter = {};
+exports.twitter.consumer_key = 'JSwd0sFkVGunfvAwrvSlECZEB'
+exports.twitter.consumer_secret = 'Ezz0OHpFylLSEYrJOmtk7txRyxZMjIoguCUFNEer3Lh6CeNRmu'
+exports.twitter.token = '117047483-ejjH2c3lR9gKHy0Z4uiBsQppJIDS6z8U7MHkNjlA'
+exports.twitter.token_secret = 'PmZofv32MoNs1MUZcvlSwzOFG4O6avsQqaAbr8eAGn3Jr'
+
+exports.tags = 'opensource'
+exports.port = 9090
+
+exports.dbhost = 'localhost:27017/hypewall' 
--- server.js
+++ server.js
@@ -1,26 +1,26 @@
+var config = require('./config.js')
+
 var express = require('express');
 var app = express();
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/hypewall');
+var db = monk(config.dbhost);
 
 app.use("/template", express.static(__dirname + '/template'));
 app.use("/frontend_libs", express.static(__dirname + '/frontend_libs'));
 
-var server = app.listen(9090, function() {
+var server = app.listen(config.port, function() {
   console.log('Listening on port %d', server.address().port);
 });
 var io = require('socket.io')(server);
 
 app.get('/', function(req, res){
-  res.sendfile(__dirname + '/template/main.html');
+  res.sendFile(__dirname + '/template/main.html');
   var collection = db.get('tweets');
 
   io.on('connection', function (socket) {
     collection.find({},{},function(e,docs){
-        console.log(docs);
         for (index = 0; index < docs.length; ++index) {
-          console.log(docs[index]);
           socket.emit('news',docs[index]);
         }
     });
@@ -29,18 +29,12 @@ app.get('/', function(req, res){
 });
 
 var Twitter = require('node-tweet-stream');
-var t = new Twitter({
-  consumer_key: 'JSwd0sFkVGunfvAwrvSlECZEB',
-  consumer_secret: 'Ezz0OHpFylLSEYrJOmtk7txRyxZMjIoguCUFNEer3Lh6CeNRmu',
-  token: '117047483-ejjH2c3lR9gKHy0Z4uiBsQppJIDS6z8U7MHkNjlA',
-  token_secret: 'PmZofv32MoNs1MUZcvlSwzOFG4O6avsQqaAbr8eAGn3Jr'
-});
-t.track('opensource')
+var t = new Twitter(config.twitter);
+t.track(config.tags)
 
 io.on('connection', function (socket) {
   var tweetCollection = db.get('tweets');
   t.on('tweet', function (tweet) {
-    console.log('New tweet: ', tweet.text)
     socket.emit('news', tweet);
     tweetCollection.insert(tweet);
   })
